b91044eff05a37731e33cea444cf58ee34eddcf5
Maju Prado <m.prado@aluno.ifsp.edu.br>
2023-05-16 11:04:02 -0300

    Inclui models Question e Choice

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+
+
+    #no terminal:
+    # python manage.py migrate
+
+    #configurar o módulo polls p ser reconhecido mo arquivo pasta-projeeto/settings.py incluir o módulocomo INSTALLED_APPS
+    # python manage.py makemigrations
