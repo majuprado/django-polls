5615e3247a144182e1f665cfca45fa4f17ab7d9d
Maju Prado <m.prado@aluno.ifsp.edu.br>
2023-06-20 11:01:43 -0300

    config Admin

--- polls/urls.py
+++ polls/urls.py
@@ -1,12 +1,38 @@
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path("polls/", include("polls.urls")),
+    path("admin/", admin.site.urls),
+]
+
+
+
 from django.urls import path
 
 from . import views
 
 urlpatterns = [
+    # ex: /polls/
     path("", views.index, name="index"),
-    path('admin/', admin.site.urls)
+    # ex: /polls/5/
+    path("<int:question_id>/", views.detail, name="detail"),
+    # ex: /polls/5/results/
+    path("<int:question_id>/results/", views.results, name="results"),
+    # ex: /polls/5/vote/
+    path("<int:question_id>/vote/", views.vote, name="vote"),
+    path('perguntas', views.ultimas_perguntas, name='ultimas_perguntas'),
 ]
-urlpatterns = [
-   ...
-   path('pergunta/<int:question_id>', views.exibe_questao, name='exibe_questao')
-]
+
+from django.http import HttpResponse
+
+from .models import Question
+
+
+def index(request):
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    output = ", ".join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+
+# Leave the rest of the views (detail, results, vote) unchanged
--- polls/views.py
+++ polls/views.py
@@ -21,4 +21,21 @@ def exibe_questao(request, question_id):
         return HttpResponse(questao.question_text)
     
     return HttpResponse('Não existe questão a exibir')
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+
+def ultimas_perguntas(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/perguntas.html', context)
     
