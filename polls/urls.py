2eab62b18952e1ce78ddcf604a76c08dcadb9a2f
Maju Prado <m.prado@aluno.ifsp.edu.br>
2023-06-28 08:54:21 -0300

    template

--- polls/urls.py
+++ polls/urls.py
@@ -1,18 +1,13 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.urls import path
+from . import views
+from django.http import HttpResponse
+from .models import Question
 
 urlpatterns = [
     path("polls/", include("polls.urls")),
     path("admin/", admin.site.urls),
-]
-
-
-
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
     # ex: /polls/
     path("", views.index, name="perguntas"),
     # ex: /polls/5/
@@ -23,16 +18,6 @@ urlpatterns = [
     path("<int:question_id>/vote/", views.vote, name="vote"),
     path('perguntas', views.ultimas_perguntas, name='ultimas_perguntas'),
 ]
-
-from django.http import HttpResponse
-
-from .models import Question
-
-
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    output = ", ".join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
 
 
 # Leave the rest of the views (detail, results, vote) unchanged
