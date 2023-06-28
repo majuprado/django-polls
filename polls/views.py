17eaf5baf8759b7800e15ce72ca7c21b1708660b
Maju Prado <m.prado@aluno.ifsp.edu.br>
2023-06-28 08:19:01 -0300

    correção de código

--- polls/views.py
+++ polls/views.py
@@ -38,4 +38,9 @@ def ultimas_perguntas(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/perguntas.html', context)
+
+def index(request):
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    output = ", ".join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
     
